260415ce706116af844b19e886f7f974a7d0e2df
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2024-09-23 22:56:37 +0530

    modify site header and footer

--- Frontend/Assets/JS/siteHeaderFooter.js
+++ Frontend/Assets/JS/siteHeaderFooter.js
@@ -1,11 +1,17 @@
-fetch('../Components/header.html')
+function loadHeaderFooter() {
+    fetch('../Components/header.html')
     .then(header => header.text())
     .then(text => {
         document.getElementById('header-container').innerHTML = text;
         });
 
-fetch('../Components/footer.html')
-    .then(footer => footer.text())
-    .then(text => {
-        document.getElementById('footer-container').innerHTML = text;
-        });
+    fetch('../Components/footer.html')
+        .then(footer => footer.text())
+        .then(text => {
+            document.getElementById('footer-container').innerHTML = text;
+            });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadHeaderFooter();
+  });
