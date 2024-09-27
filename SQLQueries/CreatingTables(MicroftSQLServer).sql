ac4ea622e98b2ed9d7bdc371798acba53505de68
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2024-09-27 19:52:54 +0530

    update sqlcreatetables

--- SQLQueries/CreatingTables(MicroftSQLServer).sql
+++ SQLQueries/CreatingTables(MicroftSQLServer).sql
@@ -1,3 +1,4 @@
+/*Creating Policy table*/
 CREATE TABLE Policy (
 	PolicyId INT IDENTITY(1,1),   /*IDENTITY for Microsoft SQL server.  AUTO_INCREMENT for MySQL*/
 	PolicyType VARCHAR (50),
@@ -5,5 +6,13 @@ CREATE TABLE Policy (
 	Premium INT,
 	
 )
+ALTER TABLE Policy ADD CONSTRAINT policy_pk_policyId PRIMARY KEY (PolicyId); 
 
-ALTER TABLE Policy ADD CONSTRAINT pk_policyId PRIMARY KEY (PolicyId); 
+/*Creating Policy Benefits table which refers to Policy table*/
+CREATE TABLE Policy_Benefits(
+	PolicyId INT,
+	Benefit VARCHAR(200),
+)
+ALTER TABLE Policy_Benefits ADD CONSTRAINT policyBenefits_fk_policyId FOREIGN KEY(PolicyId) REFERENCES Policy(PolicyId);
+
+
