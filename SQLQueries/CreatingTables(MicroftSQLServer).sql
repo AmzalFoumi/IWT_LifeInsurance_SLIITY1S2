0acc32948ff3e207b521658f6595057281153b9e
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2024-09-29 23:16:22 +0530

    update sqlcreatetables

--- SQLQueries/CreatingTables(MicroftSQLServer).sql
+++ SQLQueries/CreatingTables(MicroftSQLServer).sql
@@ -4,15 +4,31 @@ CREATE TABLE Policy (
 	PolicyType VARCHAR (50),
 	Coverage INT,
 	Premium INT,
-	
+	CONSTRAINT policy_pk_policyId PRIMARY KEY (PolicyId)
 )
-ALTER TABLE Policy ADD CONSTRAINT policy_pk_policyId PRIMARY KEY (PolicyId); 
 
 /*Creating Policy Benefits table which refers to Policy table*/
 CREATE TABLE Policy_Benefits(
 	PolicyId INT,
 	Benefit VARCHAR(200),
+	CONSTRAINT policyBenefits_fk_policyId FOREIGN KEY(PolicyId) REFERENCES Policy(PolicyId)
 )
-ALTER TABLE Policy_Benefits ADD CONSTRAINT policyBenefits_fk_policyId FOREIGN KEY(PolicyId) REFERENCES Policy(PolicyId);
 
+CREATE TABLE Policyholder (
+	PolicyholderId INT,
+	PolicyId INT,
+	NIC VARCHAR(20) UNIQUE NOT NULL,
+	Name VARCHAR(100),
+	DOB DATE,   /*YYYY.MM.DD*/
+	Income INT,
+	Street VARCHAR(200),
+	Postal_Code INT,
+	City VARCHAR(20),
+	Province VARCHAR(25),
+	Country VARCHAR(50),
+	Username VARCHAR(20) UNIQUE NOT NULL,
+	Password VARCHAR(20) NOT NULL,
+	CONSTRAINT policyholder_pk_policyholderId PRIMARY KEY (PolicyholderId),
+	CONSTRAINT policyholder_fk_policyId FOREIGN KEY(PolicyId) REFERENCES Policy(PolicyId)
+)
 
