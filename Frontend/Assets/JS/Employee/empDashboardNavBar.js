382eabfb975e71858b5a429997fb8356253ecadd
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2024-10-03 17:02:07 +0530

    emp dashbpard structure done

---
+++ Frontend/Assets/JS/Employee/empDashboardNavBar.js
@@ -0,0 +1,17 @@
+function navBarAndHeader() {
+    fetch('../../Components/EmpDashboard/empDashboardNavBar.html')
+        .then(navBar => navBar.text())
+        .then(text => {
+            document.getElementById('leftNavbar').innerHTML = text;
+            });
+
+    fetch('../../Components/EmpDashboard/empDashboardNavBar.html')
+        .then(header => header.text())
+        .then(text => {
+            document.getElementById('topNavbar').innerHTML = text;
+            });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    navBarAndHeader();
+  });
